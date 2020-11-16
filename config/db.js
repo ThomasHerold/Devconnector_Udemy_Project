90786887bbca52021c0272dce80bc25040ae98ab
Thomas Herold <thomas.herold95@gmail.com>
2020-11-15 20:07:15 -0500

    Define test routes

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// Config allows for global variable usage. Required in the URI for DB connection
+const mongoose = require('mongoose');
+const config = require ('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+       await mongoose.connect(db, {
+           useNewUrlParser: true,
+           useUnifiedTopology: true
+       });
+       console.log('MongoDB Connected')
+    } catch (err) {
+        console.error(err.message);
+        // If connection fails, we want to exit the process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
