6967a0cee3b9fa0e881430401b3d8958db06242b
Thomas Herold <thomas.herold95@gmail.com>
2020-11-28 20:34:17 -0500

    Client setup and initial landing page

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import './App.css';
+
+const App = () => {
+ return (
+  <>
+    <Navbar />
+    <Landing />
+  </>
+ );
+};
+
+export default App;
